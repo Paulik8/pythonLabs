679821ef12345cc1d8f191affc190390e9a317d1
Paulik8 <ikhotin@mail.ru>
2017-12-01 02:35:03 +0300

    lab2

---
+++ lab2/arr_algs.py
@@ -0,0 +1,14 @@
+mas = [0, 4, 10, 6, 657, 35, 3456, 2]
+
+def minimum(mas):
+    return min(mas)
+
+def sa(mas):
+    sum = 0
+    for n in mas:
+        sum = sum + n
+    return sum/len(mas)
+
+
+print (minimum(mas))
+print (sa(mas))
---
+++ lab2/str_algs.py
@@ -0,0 +1,5 @@
+st = "abcdef"
+def rev(str):
+    return st[::-1]
+
+print(rev(st))
